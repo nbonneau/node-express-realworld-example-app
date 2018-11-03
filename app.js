4e4391087d791bf02ab5c816e9eef6310c9fe88c
bonneau <nicolas.bonneau.vlb@gmail.com>
2018-11-03 14:41:06 +0100

    remove mongoose connection

--- app.js
+++ app.js
@@ -30,13 +30,14 @@ if (!isProduction) {
     app.use(errorhandler());
 }
 
+/*
 if (isProduction) {
     mongoose.connect(process.env.MONGODB_URI);
 } else {
     mongoose.connect('mongodb://localhost/conduit');
     mongoose.set('debug', true);
 }
-
+*/
 require('./models/User');
 require('./models/Article');
 require('./models/Comment');
